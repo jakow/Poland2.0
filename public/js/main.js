12f8be66c09377c43b170126aab4c9d7391dc8e9
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2016-09-12 21:39:04 +0100

    Hash scroll anim added

--- public/js/main.js
+++ public/js/main.js
@@ -186,5 +186,24 @@ function animateError(errorText) {
 });
 
 
+//  hash fragment change animation
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - ($(window).width()  < 720 ? 48 : 64)
+        }, 450);
+        return false;
+      }
+    }
+  });
+});
+
+
+
 
 
