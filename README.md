a42969f81635632f0567ac8f6df5735fdc639484
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-04-26 12:57:55 +0200

    Fix merge conflicts?

---
+++ README.md
@@ -0,0 +1,3 @@
+# Poland 2.0 v2
+
+The redesign of the backend and the frontend happens on this branch.
--- routes/helpers/cloudinary.js
+++
@@ -1,9 +0,0 @@
-module.exports = { 
-    scaleImg: function(photoUrl, width) {
-    if (typeof(photoUrl) === 'string')
-      return photoUrl.replace('upload/', `upload/c_scale,w_${width}/`);
-    else
-      // throw new Error('[scale img] photoUrl must be string');
-      return ''; // fail silently
-  }
-}
