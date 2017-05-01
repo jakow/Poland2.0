31f101319f6070b7de4bdb9f7b8342abdcf35a0d
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-05-01 12:57:53 +0200

    Fix deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jshintReporter = require('jshint-stylish');
 var watch = require('gulp-watch');
-var shell = require('gulp-shell')
 var sass = require('gulp-sass');
-var wiredep = require('wiredep').stream;
-var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
 	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
@@ -24,26 +19,6 @@ var paths = {
 
 };
 
-// gulp.task('wiredep', function () {
-//   gulp.src(paths.jade)
-//     .pipe(wiredep({
-//     	ignorePath: '../../../bower_components/'
-//     }))
-//     .pipe(gulp.dest('./templates'));
-// });
-// gulp lint
-gulp.task('lint', function(){
-	gulp.src(paths.src)
-		.pipe(jshint({esversion:6}))
-		.pipe(jshint.reporter(jshintReporter));
-});
-
-// gulp watcher for lint
-gulp.task('watch:lint', function () {
-	gulp.watch(paths.src, ['lint']);
-});
-
-
 gulp.task('watch:sass', function () {
 	gulp.watch(paths.style.all, ['sass']);
 });
@@ -56,11 +31,8 @@ gulp.task('sass', function(){
 });
 
 
-gulp.task('runKeystone', shell.task('node keystone.js'));
 gulp.task('watch', [
-
   'watch:sass',
-
   'watch:lint'
 ]);
 
