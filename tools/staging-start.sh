df8577064e1731636edb9d8293e3b0727fe7ff00
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-06-23 14:52:54 +0200

    Still bugs present

--- tools/staging-start.sh
+++ tools/staging-start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# get environment variables
+source /etc/profile.d/poland20.sh
 yarn install --production=true
 NODE_ENV=staging node_modules/.bin/pm2 start build/index.js --name staging
