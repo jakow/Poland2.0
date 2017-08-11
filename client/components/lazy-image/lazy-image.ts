8d4470f24980c24afc2c9a7b4f670be7b4c55d44
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-08-11 14:26:53 +0100

    Refactor lazy loading images

--- client/components/lazy-image/lazy-image.ts
+++ client/components/lazy-image/lazy-image.ts
@@ -10,16 +10,16 @@ class LazyImage {
   public readonly originalSrc: string;
   // public readonly yPosition: number;
   private preview: HTMLImageElement;
-  private original: HTMLImageElement;
 
   constructor(private container: HTMLElement) {
     this.method = this.container.dataset.method;
     this.preview = this.container.querySelector('img.preview') as HTMLImageElement;
-    this.previewSrc = this.preview.src;
-    this.original = this.container.querySelector('img.original') as HTMLImageElement;
+    if (this.preview) {
+      this.previewSrc = this.preview.src;
+      this.showPreview();
+    }
     this.originalSrc = this.container.dataset.src;
     // this.yPosition = this.container.offsetTop;
-    this.showPreview();
     if (this.method === 'immediate') {
       this.showOriginal();
     }
@@ -36,8 +36,14 @@ class LazyImage {
   }
 
   public async showOriginal() {
-    await this.imgLoaded(this.originalSrc, this.original);
-    this.container.classList.add('loaded');
+
+    const img = await this.imgLoaded(this.originalSrc);
+    img.classList.add('original');
+    img.alt = this.container.dataset.alt;
+    img.setAttribute('property', this.container.dataset.property);
+    this.container.appendChild(img);
+    // animate on next frame
+    setTimeout(() => this.container.classList.add('loaded'), 1000 / 60);
   }
   /**
    * The Lazy Image module will automatically bootstrap images that exist in the document.
@@ -45,6 +51,7 @@ class LazyImage {
    */
   public register() {
     // TODO: implementation
+    // TODO: Dynamically registered images should use a priority queue to maintain yPosition order efficiently
     throw new Error("not implemented");
   }
 
@@ -61,23 +68,29 @@ class LazyImage {
 }
 }
 
-
 export function init() {
   const containers: HTMLElement[] = Array.prototype.slice.call(document.querySelectorAll('.lazy-img'));
   for (const container of containers) { // tslint disable-line
     lazyImages.push(new LazyImage(container));
   }
-  scrollTriggeredQueue = lazyImages.filter( (img) => img.method === 'scroll')
-  // sort ascending by Y position. The initial sort happens before images are loaded
-  // but should be a good indication of the actual dom position.
-    .sort((a, b) => a.yPosition - b.yPosition);
-  /* The scroll event is expensive because it always reads the current offsetTop of elements
-   * rather than doing it once for each iamge. This is because dom elements may expand in which case
-   * the images may not need to be loaded anymore. Because it is computationally heavy it is really
-   * important that the event is throttled.
-   */
+  // sort ascending by Y position
+  scrollTriggeredQueue = lazyImages.filter((img) => img.method === 'scroll').sort((a, b) => a.yPosition - b.yPosition);
 
-  // feature detect
+  /* The scroll event is expensive because it always reads the current offsetTop of elements
+  * rather than doing it once for each iamge. This is because dom elements may expand in which case
+  * the images may not need to be loaded anymore. Because it is computationally heavy it is really
+  * important that the event is throttled.
+  */
+  onScroll = throttle(showNextOnScroll, 500);
+  window.addEventListener('scroll', onScroll, (supportsPassiveEvents() ? {passive: true} : undefined) as any);
+  showNextOnScroll();
+  return lazyImages;
+}
+
+/**
+ * Detect passive events which never prevent default. Scrolling is smoother if scroll event is passive
+ */
+function supportsPassiveEvents() {
   let supportsPassive = false;
   try {
     const opts = Object.defineProperty({}, 'passive', {
@@ -87,30 +100,25 @@ export function init() {
     });
     window.addEventListener("test", null, opts);
   } catch (e) {
-    console.warn('No support for passive scroll handlers');
+    // pass
   }
-
-  onScroll = throttle(showNextOnScroll, 500);
-  window.addEventListener('scroll', onScroll, (supportsPassive ? {passive: true} : false) as any);
-  showNextOnScroll();
-  return lazyImages;
+  // console.info('Support for passive scroll handlers', supportsPassive);
+  return supportsPassive;
 }
 
 /**
  * Handle showing of scroll-triggered LazyImages
- * @param ev The UI event that triggers the loading. Currently unused.
+ * @param ev The scroll event that triggers the loading.
  */
 function showNextOnScroll(ev?: UIEvent) {
   const threshold = document.body.scrollTop + window.innerHeight;
   // because the array is sorted w.r.t. y position then we can just select the images
   // that are above the threshold and splice them from array;
-  // console.log('threshold', threshold);
-  // console.log(scrollTriggeredQueue.map((e) => e.yPosition));
-  // console.log(`[lazy image] Images left: ${scrollTriggeredQueue.length}`);
   const toBeShown = takeWhile(scrollTriggeredQueue, (img) => img.yPosition < threshold);
   toBeShown.forEach((img) => img.showOriginal());
   // remove the shown images from the shown array
   scrollTriggeredQueue.splice(0, toBeShown.length);
+  if (scrollTriggeredQueue.length === 0) {
+    window.removeEventListener('scroll', onScroll);
+  }
 }
-
-
