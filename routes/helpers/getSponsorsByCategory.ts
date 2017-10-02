f4ed39f9aeba5fd5e5b68baed75a4c5a00fb011c
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-10-02 19:57:06 +0100

    reverse sort order

--- routes/helpers/getSponsorsByCategory.ts
+++ routes/helpers/getSponsorsByCategory.ts
@@ -8,6 +8,6 @@ export default async function getSponsorsByCategory(sponsorFilter: any = {}, cat
   const sponsors = await list<Sponsor>('Sponsor').model
     .find({category: { $ne: null }, ...sponsorFilter}).exec();
   const sponsorCategories = await list<SponsorCategory>('SponsorCategory').model
-    .find(categoryFilter).sort('-sortOrder').exec();
+    .find(categoryFilter).sort('sortOrder').exec();
   return reversePopulate(sponsorCategories, 'sponsors', sponsors, 'category');
 }
