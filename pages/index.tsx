8f1dbeb2b86e53fe2ab833da5ecb028c33252ee7
Artur96 <artur@komoter.pl>
2018-09-10 23:44:30 +0100

    Added top navigation bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import 'p20-components/bundle.css';
-import { Container } from 'p20-components';
+import { Container, TopNavigation, rhythm } from 'p20-components';
+import { NavItem } from '../routes/middleware';
 
-const App: React.StatelessComponent = () => (
-  <Container>
-    Hello World
-  </Container>
-);
+class App extends React.Component<{ navLinks: NavItem[] }> {
+  static async getInitialProps() {
+    const navLinks = await fetch('http://localhost:9009/middleware/getNavLinks');
+    const json = await navLinks.json();
+    return { ...json };
+  }
+
+  render = () => (
+    <React.Fragment>
+      <TopNavigation items={this.props.navLinks}/>
+      <Container style={{ marginTop: rhythm(3) }}>
+        Hello World
+      </Container>
+    </React.Fragment>
+  )
+};
 
 export default App;
