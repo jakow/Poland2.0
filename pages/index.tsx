27f2df20d6e990568364864a6d0f051c6a18da6b
Artur96 <artur@komoter.pl>
2018-09-13 22:59:00 +0100

    Added Home page stub

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,8 @@
-import * as React from 'react';
-import 'p20-components/bundle.css';
-import { Container, TopNavigation, rhythm } from 'p20-components';
-import { NavItem } from '../routes/middleware';
+import React from 'react';
+import withDefault from './_app';
 
-class App extends React.Component<{ navLinks: NavItem[] }> {
-  static async getInitialProps() {
-    const navLinks = await fetch('http://localhost:9009/middleware/getNavLinks');
-    const json = await navLinks.json();
-    return { ...json };
-  }
+const Home: React.StatelessComponent = () => (
+  <div>Hello World</div>
+);
 
-  render = () => (
-    <React.Fragment>
-      <TopNavigation items={this.props.navLinks}/>
-      <Container style={{ marginTop: rhythm(3) }}>
-        Hello World
-      </Container>
-    </React.Fragment>
-  )
-};
-
-export default App;
+export default withDefault(Home);
