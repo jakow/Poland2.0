eba91fa7c7696e83be0dab313e6d7d2517786ed1
Artur96 <artur@komoter.pl>
2018-09-15 22:29:20 +0100

    Default page HOC now passes down middleware objects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,42 @@
 import React from 'react';
-import { Container, TopNavigation, rhythm } from 'p20-components';
+import { TopNavigation, rhythm } from 'p20-components';
 import { NavItem } from '../routes/middleware';
 import Footer from '../components/Footer';
 import { ContentControl, Edition } from '../models';
 
-interface Props {
+export interface DefaultProps {
   contentControl?: ContentControl;
   currentEdition?: Edition;
   navLinks?: NavItem[];
 }
 
-const withDefault = (Page: React.ComponentType) => (class extends React.Component<Props> {
-  static async getInitialProps() {
-    const middleware = await fetch('http://localhost:9009/middleware')
-      .then(data => data.json());
-    return { ...middleware };
-  }
+const withDefault = (Page: React.ComponentType<DefaultProps>) =>
+  (class extends React.Component<DefaultProps> {
+    static async getInitialProps() {
+      const middleware = await fetch('http://localhost:9009/middleware')
+        .then(data => data.json());
+      return { ...middleware };
+    }
 
-  render = () => (
-    <React.Fragment>
-      <TopNavigation items={this.props.navLinks}/>
-      <Container style={{ marginTop: rhythm(3) }}>
-        <Page/>
-      </Container>
-      <Footer
-        bylawLink={this.props.contentControl.bylawLink}
-        facebookUrl={this.props.contentControl.facebookUrl}
-        linkedinUrl={this.props.contentControl.linkedinUrl}
-        twitterUrl={this.props.contentControl.twitterUrl}
-        videoChannelUrl={this.props.contentControl.videoChannelUrl}
-      />
-    </React.Fragment>
-  )
-});
+    render = () => (
+      <React.Fragment>
+        <TopNavigation items={this.props.navLinks}/>
+        <main style={{ marginTop: rhythm(3) }}>
+          <Page
+            contentControl={this.props.contentControl}
+            currentEdition={this.props.currentEdition}
+          />
+        </main>
+        <Footer
+          bylawLink={this.props.contentControl.bylawLink}
+          facebookUrl={this.props.contentControl.facebookUrl}
+          linkedinUrl={this.props.contentControl.linkedinUrl}
+          twitterUrl={this.props.contentControl.twitterUrl}
+          videoChannelUrl={this.props.contentControl.videoChannelUrl}
+        />
+      </React.Fragment>
+    )
+  }
+);
 
 export default withDefault;
