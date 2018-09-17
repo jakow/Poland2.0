b7677874d38a5187c9e20cbe0cac97f3ab65824e
Artur96 <artur@komoter.pl>
2018-09-17 23:46:41 +0100

    Added back speakers, agenda and sponsors navigation links, cleanup

--- routes/middleware.ts
+++ routes/middleware.ts
@@ -19,27 +19,21 @@ export interface NavItem {
 middleware.get('/', async (req: Request, res: Response, next: NextFunction) => {
   const contentControl = await list<ContentControl>('ContentControl').model.findOne().exec();
   const currentEdition = await list<Edition>('Edition').model.findOne({ current: true }).exec();
-  // const staticPages = await list<StaticPage>('StaticPage').model
-  //   .find({ active: true, showInMenu: true })
-  //   .select({ name: true, route: true }).exec();
 
   const navLinks: NavItem[] = [
     { title: 'About', url: '/about' },
-    // ...contentControl.showSpeakers ? [{ title: 'Speakers', url: '/#speakers' }]
-    //   : [],
-    // ...contentControl.showAgenda ? [{ title: 'Agenda', url: '/#agenda' }]
-    //   : [],
-    // ...contentControl.showSponsors ? [{ title: 'Partners', url: '/#partners' }]
-    //   : [],
+    ...contentControl.showSpeakers ? [{ title: 'Speakers', url: '/#speakers' }]
+      : [],
+    ...contentControl.showAgenda ? [{ title: 'Agenda', url: '/#agenda' }]
+      : [],
+    ...contentControl.showSponsors ? [{ title: 'Partners', url: '/#partners' }]
+      : [],
     { title: 'Past Editions', url: '/past-editions' },
     { title: 'empowerPL', url: '/empowerPL' },
     ...contentControl.tickets.live && contentControl.tickets.url ?
-      [<NavItem>{ title: 'Tickets', url: contentControl.tickets.url, style: 'button' }]
+      [<NavItem>{ title: 'Buy Tickets', url: contentControl.tickets.url, style: 'button' }]
       : []
   ];
-  // const staticPageRoutes: NavItem[] = staticPages.map(p => ({
-  //   title: p.name, url: p.route
-  // }));
 
   res.json({
     contentControl,
