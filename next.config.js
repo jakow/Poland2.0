b39f0d2ab590ebcccb38e85771463a2477167aa9
Artur96 <artur@komoter.pl>
2018-09-18 21:37:03 +0100

    Added IntersectionObserver polyfill for modern IE11 (i.e. Safari)

--- next.config.js
+++ next.config.js
@@ -2,12 +2,26 @@ import * as withTypescript from '@zeit/next-typescript';
 import * as withCSS from '@zeit/next-css';
 import * as withOptimizedImages from 'next-optimized-images';
 
-export default withTypescript(withCSS(withOptimizedImages({
-  webpack: config => {
-    config.node = {
-      fs: 'empty'
-    };
+export default
+  withTypescript(
+  withCSS(
+  withOptimizedImages({
+    webpack: config => {
+      config.node = {
+        fs: 'empty'
+      };
 
-    return config;
+      const original = config.entry;
+      config.entry = async () => {
+        const entries = await original();
+        if (entries['main.js'] && !entries['main.js'].includes('./polyfill.js')) {
+          entries['main.js'].unshift('./polyfill.js')
+        }
+
+        return entries;
+      }
+
+      return config;
+    }
   }
-})));
+)));
