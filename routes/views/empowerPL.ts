119bb7d3ea836c457d1efecaff38b2099ca7d5f7
Artur Komoter <artur@komoter.pl>
2019-04-14 21:09:40 +0100

    Removed old dependency for empowerPL page

--- routes/views/empowerPL.ts
+++ routes/views/empowerPL.ts
@@ -1,10 +1,5 @@
-import {View} from 'keystone';
-import {RequestHandler} from 'express';
-import resolveView from '../helpers/resolveView';
+// import {View} from 'keystone';
+import { RequestHandler } from 'express';
+// import resolveView from '../helpers/resolveView';
 
-export const empowerPL: RequestHandler = async (req, res, next) => {
-    res.locals.route = 'empowerPL';
-    const view = new View(req, res);
-
-    view.render(resolveView('empowerPL'));
-  };
+export const empowerPL: RequestHandler = async (req, res, next) => {};
