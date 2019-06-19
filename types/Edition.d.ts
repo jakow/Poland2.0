3604f3d95d6a2d4ccd7df5c389c741c449c95ee6
Artur Komoter <artur@komoter.pl>
2019-06-19 17:29:31 +0100

    Added speaker categories to Edition type definition

--- types/Edition.d.ts
+++ types/Edition.d.ts
@@ -5,6 +5,7 @@ import SponsorCategory from './SponsorCategory';
 import Sponsor from './Sponsor';
 import TeamMember from './TeamMember';
 import { AgendaDay } from './Agenda';
+import SpeakerCategory from './SpeakerCategory';
 
 type Edition = {
   year?: number;
@@ -13,6 +14,7 @@ type Edition = {
   startDate: string;
   endDate: string;
   venue: Venue;
+  speakerCategories?: SpeakerCategory[];
   speakers?: Speaker[];
   sponsorCategories?: SponsorCategory[];
   sponsors?: Sponsor[];
