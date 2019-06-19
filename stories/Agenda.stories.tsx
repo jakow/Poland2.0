453306ceea724b4b9e8f9aaac961a6764610d7d9
Artur Komoter <artur@komoter.pl>
2019-06-19 14:59:30 +0100

    Fixed stories for Agenda and Banner components

--- stories/Agenda.stories.tsx
+++ stories/Agenda.stories.tsx
@@ -1,62 +1,53 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Agenda from 'components/Agenda';
-import { AgendaType, AgendaDay, EventType, EventSpeaker } from 'types/Agenda';
-import { Venue } from 'types/Venue';
 import { colors } from 'components/variables';
+import Venue from 'types/Venue';
+import Speaker from 'types/Speaker';
+import { AgendaEvent, AgendaDay } from 'types/Agenda';
 
 storiesOf('Agenda', module)
   .add('default view', () => {
 
     const place: Venue = {
       name: 'Skempton Building',
-      location: {
-        street1: 'Imperial College London',
-        street2: 'Kensington',
-        suburb: 'London',
-        postcode: 'SW7 2AZ'
-      }
+      location: 'Imperial College London\nKensington\nLondon\nSW7 2AZ'
     };
 
-    const speaker: EventSpeaker = {
+    const speaker: Speaker = {
       name: 'Janusz',
-      company: 'BCG',
+      organisation: 'BCG',
       photo: {
-        secure_url: 'pqrpqwsuvg2mynfi2pvo'
+        url: 'pqrpqwsuvg2mynfi2pvo'
       }
     };
 
-    const event: EventType = {
+    const event: AgendaEvent = {
       name: 'Why Fintechs Will Kill Banks in a Swift Move',
       type: 'Panel',
       description: 'We are proud to host top leaders from industry, government, and academia in the\
  Finance block.',
-      slug: 'dollars',
       category: {
         name: 'Finance',
         color: '#587dad',
       },
-      time: {
-        start: '2018-11-24T09:00:00Z',
-        end: '2018-11-24T11:00:00Z',
-      },
-      speakers: [speaker, speaker, speaker, speaker, speaker],
-      venue: place
+      startTime: '2018-11-24T09:00:00Z',
+      endTime: '2018-11-24T11:00:00Z',
+      speakers: [speaker, speaker, speaker, speaker, speaker]
     };
 
     const day: AgendaDay = {
       name: 'Friday, November 24th',
-      date: new Date('2018-11-24'),
       description: 'Lorem ipsum bla bla bla',
-      venue: 'Imperial College',
+      venue: place,
       events: [event, event],
     };
 
-    const agenda: AgendaType = { days: [day, day] };
+    const days = [day, day];
 
     return (
       <div style={{ backgroundColor: `${colors.lightGray}` }}>
-        <Agenda agenda={agenda}/>
+        <Agenda agendaDays={days}/>
       </div>
     );
   });
--- stories/Banner.stories.tsx
+++ stories/Banner.stories.tsx
@@ -1,26 +1,20 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Banner from 'components/Banner';
-import { Edition } from 'types/Edition';
+import Edition from 'types/Edition';
 
 storiesOf('Banner', module)
   .add('default view', () => {
     // tslint:disable
     const edition: Edition = {
-      isoString: '2018-11-23/2018-11-24',
-      dateString: '23–24 November, 2018',
+      startDate: '2019-10-25',
+      endDate: '2019-10-26',
       venue: {
         name: 'Imperial College London'
       },
-      photos: [
-        { secure_url: 'nt9wsxyxdslsbgi6j19s' },
-        { secure_url: 'i6jwnsajcihb2trnjpdv' },
-        { secure_url: 'hrp5koxx3lo8pca8vfyp' },
-        { secure_url: 'kvklnbfvy9ktokfx2ha2' }
-      ]
+      description: 'Et eaque ad animi veniam provident est quisquam. Quis repellat nemo debitis quo nesciunt nulla. Porro repellat ratione porro eos quibusdam quia consequuntur. Illo quibusdam tempora odio ut. Impedit hic amet sit fugit veritatis ipsam. Consequuntur qui quidem omnis. Est rerum qui est. Animi enim pariatur doloremque exercitationem voluptatum voluptatem est. Delectus illo nemo qui.'
     };
-    const description = 'Et eaque ad animi veniam provident est quisquam. Quis repellat nemo debitis quo nesciunt nulla. Porro repellat ratione porro eos quibusdam quia consequuntur. Illo quibusdam tempora odio ut. Impedit hic amet sit fugit veritatis ipsam. Consequuntur qui quidem omnis. Est rerum qui est. Animi enim pariatur doloremque exercitationem voluptatum voluptatem est. Delectus illo nemo qui.'
     // tslint:enable
 
-    return <Banner currentEdition={edition} description={description}/>;
+    return <Banner currentEdition={edition}/>;
   });
