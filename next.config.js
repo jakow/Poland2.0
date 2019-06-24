91d186c7e1a6cb94b8d14637e861d2e3b333312d
Artur Komoter <artur@komoter.pl>
2019-06-24 23:57:16 +0100

    Renamed API URL environment variable

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ module.exports =
       return config;
     },
     publicRuntimeConfig: {
-      host: process.env.NODE_ENV === 'production' ? process.env.WEBSITE_URL : 'http://localhost:1337',
+      host: process.env.NODE_ENV === 'production' ? process.env.API_URL : 'http://localhost:1337',
     }
   }
 )));
