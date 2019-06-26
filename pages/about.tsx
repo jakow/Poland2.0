2c5e5094b370c7645766693d40734db126c23108
Artur Komoter <artur@komoter.pl>
2019-06-26 16:51:43 +0100

    Updated conference description on About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -65,27 +65,25 @@ const About: React.StatelessComponent<DefaultPageProps> = ({ currentEdition }) =
           <h1 className={`${_stripe} ${_bold}`} style={{ marginBottom: rhythm(1.25) }}>Mission</h1>
           <h3><em>To overtake the future: innovation across industries</em></h3>
         </Center>
-        <p> {/* TODO: Put this into Keystone */}
-          Progress defines future. Scientific and technological innovation shapes our
-          everyday life. Some may argue that it is difficult to see this impact on all aspects
-          of our life. We could not disagree more. And we are here to show that innovation is the
-          omnipresent, universal language capable of connecting diverse fields together.
+        <p>
+          Progress defines future. Scientific and technological innovation shapes our everyday
+          life. Some ask how can we see impact of something that is only coming on our lives.
+          And we are here to show that innovation is the omnipresent, universal language capable
+          of connecting diverse fields together.
         </p>
         <p>
-          This November at Imperial College London, <strong>Poland 2.0</strong> will create a
-          platform for students, researchers and professionals to engage in an inspiring
-          discussion about the future direction of research, business and public policy. We
-          will prove that some seemingly distinct fields like finance, healthcare, environment,
-          or entertainment are in fact connected by one factor: spectacular breakthroughs. We
-          also think that Poland, with its fast-paced market and brilliant minds, has potential
-          to set the scene for such unique marriages.
+          This October, <strong>Poland 2.0 Summit</strong> will create a platform for students,
+          public servants and professionals to engage in an inspiring discussion about the future
+          direction of technology, innovation and sustainability. We will prove that some seemingly
+          distinct fields like finance, healthcare, gaming are in fact connected by one factor:
+          spectacular breakthroughs by no other but Poles.
         </p>
         <p>
-          Tomorrow may well be anybody’s guess, but we believe that the brilliant minds of today
-          can take the future into their own hands and shape it. At&nbsp;
-          <strong>Poland 2.0 Summit</strong>, you will have a unique chance to network with
-          like-minded peers, meet the game-changing business players, learn about crucial changes
-          across industries, and improve your career prospects, while simply having an amazing time.
+          Tomorrow may well be anybody’s guess, but we believe that the brilliant minds of today can
+          take the future into their own hands and shape it. At <strong>Poland 2.0 Summit</strong>,
+          you will have a unique chance to network with like-minded peers, meet the game-changing
+          business players during lunches, workshops and more, learn about crucial changes across
+          industries, and improve your career prospects, during this annual event.
         </p>
       </Mission>
       <TeamMembers teamMembers={currentEdition.teamMembers}/>
