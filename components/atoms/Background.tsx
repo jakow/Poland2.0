a9160665340f82311bd578138eb9d6a4f7122829
Artur Komoter <artur@komoter.pl>
2019-07-25 11:42:43 +0100

    Fixed anchors being coloured by Background

--- components/atoms/Background.tsx
+++ components/atoms/Background.tsx
@@ -2,10 +2,10 @@ import styled from '@emotion/styled-base';
 import { colors } from '../variables';
 
 const Background = styled('section')({
-  '& > *:nth-of-type(odd)': {
+  '& > *:nth-of-type(odd):not(a)': {
     backgroundColor: `${colors.gray}`
   },
-  '& > *:nth-of-type(even)': {
+  '& > *:nth-of-type(even):not(a)': {
     backgroundColor: `${colors.white}`
   }
 });
