ba2053dd61aacedb4611875699527e344d0f1905
Artur Komoter <artur@komoter.pl>
2019-08-10 22:01:09 +0200

    Disabled server-side type checking

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const dotEnv = require('dotenv').config();
+
 if (dotEnv.error) {
   throw dotEnv.error;
 }
@@ -7,34 +8,38 @@ const withOptimizedImages = require('next-optimized-images');
 
 const localhost = 'http://localhost:1337';
 
-module.exports =
-  // withCSS(
-  withOptimizedImages({
-    webpack: config => {
-      config.node = {
-        fs: 'empty'
-      };
+module.exports = withOptimizedImages({
+  webpack: (config) => {
+    config.node = {
+      fs: 'empty',
+    };
 
-      const original = config.entry;
-      config.entry = async () => {
-        const entries = await original();
-        if (entries['main.js'] && !entries['main.js'].includes('./polyfill.js')) {
-          entries['main.js'].unshift('./polyfill.js')
-        }
-
-        return entries;
+    const original = config.entry;
+    config.entry = async () => {
+      const entries = await original();
+      if (entries['main.js'] && !entries['main.js'].includes('./polyfill.js')) {
+        entries['main.js'].unshift('./polyfill.js');
       }
 
-      return config;
-    },
-    env: {
-      stripeApiKey: process.env.NODE_ENV !== 'production' ? process.env.STRIPE_PK_TEST : process.env.STRIPE_PK_LIVE,
-    },
-    serverRuntimeConfig: {
-      host: process.env.NODE_ENV !== 'development' ? 'http://api:1337' : localhost
-    },
-    publicRuntimeConfig: {
-      host: process.env.NODE_ENV !== 'development' ? process.env.API_URL : localhost
-    }
-  }
-);//);
+      return entries;
+    };
+
+    config.plugins = config.plugins.filter((plugin) => {
+      if (plugin.constructor.name === 'ForkTsCheckerWebpackPlugin') return false;
+      return true;
+    });
+
+    return config;
+  },
+  env: {
+    stripeApiKey: process.env.NODE_ENV !== 'production'
+      ? process.env.STRIPE_PK_TEST
+      : process.env.STRIPE_PK_LIVE,
+  },
+  serverRuntimeConfig: {
+    host: process.env.NODE_ENV !== 'development' ? 'http://api:1337' : localhost,
+  },
+  publicRuntimeConfig: {
+    host: process.env.NODE_ENV !== 'development' ? process.env.API_URL : localhost,
+  },
+});
