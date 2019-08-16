0ed81eaa8f6d446163616fdf8ed14805e0b50d33
Artur Komoter <artur@komoter.pl>
2019-08-16 19:02:57 +0100

    Invalid tickets are now automatically removed from basket

--- components/organisms/Basket/logic.ts
+++ components/organisms/Basket/logic.ts
@@ -2,19 +2,38 @@ import React from 'react';
 import TicketType from '../../../types/TicketType';
 import { toGBP } from '../../../helpers/currency';
 
-export const getBasket = () => JSON.parse(localStorage.getItem('basket') || '{}');
+export const getBasket = (ticketTypes?: TicketType[]) => {
+  const storage = localStorage.getItem('basket');
+  if (!storage) {
+    return {};
+  }
 
-export const getTotalAmount = (ticketTypes: TicketType[], basket: any) =>
-  ticketTypes.reduce(
-    (sum, ticketType) =>
-      basket[ticketType.id]
-      ? sum + basket[ticketType.id] * ticketType.price
-      : sum,
-    0
+  const basket = JSON.parse(storage);
+  if (!ticketTypes) {
+    return basket;
+  }
+
+  ticketTypes.forEach((ticketType) => {
+    if ((basket[ticketType.id] && !ticketType.active) || (basket[ticketType.id] > ticketType.quantity)) {
+      delete basket[ticketType.id];
+    }
+  });
+
+  localStorage.setItem('basket', JSON.stringify(basket));
+
+  return basket;
+};
+
+export const getTotalAmount = (ticketTypes: TicketType[], basket: any) => ticketTypes.reduce(
+  (sum, ticketType) => (basket[ticketType.id]
+    ? sum + basket[ticketType.id] * ticketType.price
+    : sum
+  ), 0,
 );
 
-export const getFormattedTotalAmount = (ticketTypes: TicketType[], basket: any) =>
-  toGBP(getTotalAmount(ticketTypes, basket));
+export const getFormattedTotalAmount = (ticketTypes: TicketType[], basket: any) => (
+  toGBP(getTotalAmount(ticketTypes, basket))
+);
 
 export const basketEffect = (setBasket: React.Dispatch<any>) => () => {
   const handleBasketChange = () => {
@@ -22,7 +41,7 @@ export const basketEffect = (setBasket: React.Dispatch<any>) => () => {
     setBasket(storage);
   };
 
-  addEventListener('storage', handleBasketChange);
+  window.addEventListener('storage', handleBasketChange);
 
-  return () => removeEventListener('storage', handleBasketChange);
+  return () => window.removeEventListener('storage', handleBasketChange);
 };
