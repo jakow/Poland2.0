7f39a08d5d72600a16e2d8b4e13c15d8312d87fd
Artur Komoter <artur@komoter.pl>
2019-08-21 20:03:57 +0100

    Added Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,48 +1,55 @@
 import 'isomorphic-unfetch';
-import Document, { Head, Main, NextScript, Html } from 'next/document';
+import Document, {
+  Head, Main, NextScript, Html,
+} from 'next/document';
 
 export default class extends Document {
   render() {
     return (
       <Html lang="en-GB">
         <Head>
-          {/* tslint:disable */}
+          {/* eslint-disable */}
           <meta charSet="utf-8"/>
-          <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
-          <meta name="description" content="Annual Polish student conference, tackling entrepreneurship, innovation and change across industries."/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="description" content="Annual Polish student conference, tackling entrepreneurship, innovation and change across industries." />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/static/images/favicons/apple-touch-icon-57x57.png"/>
-          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/images/favicons/apple-touch-icon-114x114.png"/>
-          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/images/favicons/apple-touch-icon-72x72.png"/>
-          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/images/favicons/apple-touch-icon-144x144.png"/>
-          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/static/images/favicons/apple-touch-icon-120x120.png"/>
-          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/static/images/favicons/apple-touch-icon-152x152.png"/>
-          <link rel="icon" type="image/png" href="/static/images/favicons/favicon-32x32.png" sizes="32x32"/>
-          <link rel="icon" type="image/png" href="/static/images/favicons/favicon-16x16.png" sizes="16x16"/>
-          <link rel="mask-icon" href="/static/images/favicons/mask-icon.svg" color="#C53D56"/>
+          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/static/images/favicons/apple-touch-icon-57x57.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/images/favicons/apple-touch-icon-114x114.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/images/favicons/apple-touch-icon-72x72.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/images/favicons/apple-touch-icon-144x144.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/static/images/favicons/apple-touch-icon-120x120.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/static/images/favicons/apple-touch-icon-152x152.png" />
+          <link rel="icon" type="image/png" href="/static/images/favicons/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/static/images/favicons/favicon-16x16.png" sizes="16x16" />
+          <link rel="mask-icon" href="/static/images/favicons/mask-icon.svg" color="#C53D56" />
 
-          <link href="https://unpkg.com/@blueprintjs/icons@^3.4.0/lib/css/blueprint-icons.css" rel="stylesheet"/>
-          <link rel="stylesheet" href="/static/fonts/league-spartan.css"/>
+          <link href="https://unpkg.com/@blueprintjs/icons@^3.4.0/lib/css/blueprint-icons.css" rel="preload" as="style" />
+          <link rel="stylesheet" href="/static/fonts/league-spartan.css" />
 
-          <meta name="application-name" content="Poland 2.0"/>
-          <meta name="msapplication-TileColor" content="#FFFFFF"/>
-          <meta name="msapplication-TileImage" content="/static/images/favicons/mstile-144x144.png"/>
+          <meta name="application-name" content="Poland 2.0" />
+          <meta name="msapplication-TileColor" content="#FFFFFF" />
+          <meta name="msapplication-TileImage" content="/static/images/favicons/mstile-144x144.png" />
 
-          <script src="https://js.stripe.com/v3/"/>
+          <script src="https://js.stripe.com/v3/" />
 
           <style>
-          {`@media screen and (max-width: 375px) { html { font-size: 85% !important } }`}
+            {'@media screen and (max-width: 375px) { html { font-size: 85% !important } }'}
           </style>
-          {/* tslint:enable */}
+
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146052606-1" />
+          <script src="/static/gtag.js" />
+
+          {/* eslint-enable */}
         </Head>
         <body>
           <noscript>
             This site requires JavaScript for best user experience.
             Because you have disabled JavaScript, your experience may be limited.
           </noscript>
-          <Main/>
-          <NextScript/>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
