ea1e915898bb846670f3f8fe5ba8eed3abfc18a1
Artur Komoter <artur@komoter.pl>
2019-08-28 21:54:38 +0100

    Changed footer acknowledgments...

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -11,14 +11,14 @@ const Container = styled('footer')({
   alignItems: 'center',
   position: 'relative',
   backgroundSize: 'cover',
-  backgroundPosition: 'center center', // tslint:disable-next-line
-  backgroundImage: `url('https://res.cloudinary.com/dg1royeho/image/upload/c_scale,w_1024/v1498041997/footer-img2_afjcgn.jpg')`,
-  padding: `${rhythm(2)} ${rhythm(1)}`
+  backgroundPosition: 'center center', // eslint-disable-next-line
+  backgroundImage: 'url("https://res.cloudinary.com/dg1royeho/image/upload/c_scale,w_1024/v1498041997/footer-img2_afjcgn.jpg")',
+  padding: `${rhythm(2)} ${rhythm(1)}`,
 });
 
 const Credits = styled('small')({
   lineHeight: rhythm(1),
-  textAlign: 'center'
+  textAlign: 'center',
 });
 
 const Legal = styled('div')({
@@ -26,29 +26,28 @@ const Legal = styled('div')({
   [breakpointMax('mobile')]: {
     '& > *': {
       flexBasis: '33.3%',
-      textAlign: 'center'
-    }
-  }
+      textAlign: 'center',
+    },
+  },
 });
 
 const Separator = styled('div')({
   height: 1,
   width: rhythm(8),
   marginTop: rhythm(1),
-  backgroundColor: `${colors.mediumGray}`
+  backgroundColor: `${colors.mediumGray}`,
 });
 
 const Social = styled('div')({
   display: 'flex',
-  justifyContent: 'center'
+  justifyContent: 'center',
 });
 
-const socialMedia =
-  ['facebook', 'linkedin', 'instagram', 'github'].map(media => ({
-    [`&.${media}`]: {
-      backgroundImage: `url('https://unpkg.com/simple-icons@latest/icons/${media}.svg')`
-    }
-  }));
+const socialMedia = ['facebook', 'linkedin', 'instagram', 'github'].map(media => ({
+  [`&.${media}`]: {
+    backgroundImage: `url('https://unpkg.com/simple-icons@latest/icons/${media}.svg')`,
+  },
+}));
 
 const Icon = styled('a')(
   {
@@ -62,71 +61,81 @@ const Icon = styled('a')(
     '& + &': {
       marginLeft: rhythm(0.5),
       [breakpointMin('tablet')]: {
-        marginLeft: rhythm(0.75)
-      }
-    }
+        marginLeft: rhythm(0.75),
+      },
+    },
   },
-  ...socialMedia
+  ...socialMedia,
 );
 
 const Footer: React.StatelessComponent<Urls> = ({
-  bylawUrl, privacyPolicyUrl, facebookUrl, linkedinUrl, instagramUrl, githubUrl
+  bylawUrl, privacyPolicyUrl, facebookUrl, linkedinUrl, instagramUrl, githubUrl,
 }) => (
   <Container>
     <Social>
-      {facebookUrl &&
-        <Icon
-          className="facebook"
-          href={facebookUrl}
-          rel="noopener noreferrer"
-          target="_blank"
-          title="Facebook"
-        />
+      {facebookUrl
+        && (
+          <Icon
+            className="facebook"
+            href={facebookUrl}
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Facebook"
+          />
+        )
       }
-      {githubUrl &&
-        <Icon
-          className="github"
-          href={githubUrl}
-          title="GitHub"
-          rel="noopener noreferrer"
-          target="_blank"
-        />
+      {githubUrl
+        && (
+          <Icon
+            className="github"
+            href={githubUrl}
+            title="GitHub"
+            rel="noopener noreferrer"
+            target="_blank"
+          />
+        )
       }
-      {instagramUrl &&
-        <Icon
-          className="instagram"
-          href={instagramUrl}
-          title="Instagram"
-          rel="noopener noreferrer"
-          target="_blank"
-        />
+      {instagramUrl
+        && (
+          <Icon
+            className="instagram"
+            href={instagramUrl}
+            title="Instagram"
+            rel="noopener noreferrer"
+            target="_blank"
+          />
+        )
       }
-      {linkedinUrl &&
-        <Icon
-          className="linkedin"
-          href={linkedinUrl}
-          rel="noopener noreferrer"
-          target="_blank"
-          title="LinkedIn"
-        />
+      {linkedinUrl
+        && (
+          <Icon
+            className="linkedin"
+            href={linkedinUrl}
+            rel="noopener noreferrer"
+            target="_blank"
+            title="LinkedIn"
+          />
+        )
       }
     </Social>
-    <Separator/>
+    <Separator />
     <Legal>
-      {bylawUrl && <Anchor href={bylawUrl}>By-law for Poland 2.0</Anchor>}
-      {privacyPolicyUrl &&
-        <Anchor href={privacyPolicyUrl} label="Privacy Policy" target="_blank">
-          Privacy Policy
-        </Anchor>
+      {bylawUrl && <Anchor href={bylawUrl}>By-law for Poland 2.0 Summit</Anchor>}
+      {privacyPolicyUrl
+        && (
+          <Anchor href={privacyPolicyUrl} label="Privacy Policy" target="_blank" rel="noopener noreferrer">
+            Privacy Policy
+          </Anchor>
+        )
       }
       <Anchor href="mailto:contact@poland20.com">Contact</Anchor>
     </Legal>
     <Credits>
-      &#169; {(new Date()).getFullYear()} Poland 2.0<br/>
+      &#169; {(new Date()).getFullYear()} Poland 2.0 Summit<br />
       Created by&nbsp;
-      <a href="https://github.com/jakow" target="_blank" rel="noopener">Jakub Kowalczyk</a><br/>
-      Maintained by&nbsp;
-      <a href="https://github.com/arutr" target="_blank" rel="noopener">Artur Komoter</a>
+      <a href="https://github.com/jakow" target="_blank" rel="noopener noreferrer">Jakub Kowalczyk</a><br />
+      and&nbsp;
+      <a href="https://github.com/arutr" target="_blank" rel="noopener noreferrer">Artur Komoter</a>
     </Credits>
   </Container>
 );
