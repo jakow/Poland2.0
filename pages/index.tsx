6a46d3395d73ed411f6a154b3f8be14867c81323
Artur Komoter <artur@komoter.pl>
2019-10-01 21:10:03 +0100

    Finish home page hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import Head from 'next/head';
 import styled from '@emotion/styled';
@@ -8,50 +9,79 @@ import { dateString } from '../helpers/date';
 import { DefaultPageProps, api } from './_app';
 import Background from '../components/atoms/Background';
 import Sponsors from '../components/organisms/Sponsors';
-import TicketAlert from '../components/organisms/TicketAlert';
+// import TicketAlert from '../components/organisms/TicketAlert';
 import Sponsor from '../types/Sponsor';
-import { breakpointMax, colors } from '../components/variables';
+import { breakpointMax, breakpointMin, colors } from '../components/variables';
 import { dangerousSuperscripts, rhythm } from '../components/typography';
 import { navHeight } from '../components/organisms/TopNavigation';
-import { Header1, Header2, Header3 } from '../components/atoms/Headers';
+import { Header1, Header3 } from '../components/atoms/Headers';
 import { IconLabel, TextWithIcon } from '../components/molecules/TicketTile';
 
+const informationWidth = 14;
+const desktopExcessLeft = '((100vw - 1280px) / 2)';
+
+const Information = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: `calc(100vh - ${navHeight} - ${rhythm(1)})`,
+  [breakpointMin('tabletLarge')]: {
+    width: rhythm(informationWidth),
+  },
+  [breakpointMin('desktop')]: {
+    paddingLeft: `calc(${desktopExcessLeft})`,
+    width: `calc(${rhythm(informationWidth)} + ${desktopExcessLeft})`,
+  },
+});
+
 const CoverMedia = styled('div')({
+  'video, img': {
+    width: '100%',
+    opacity: 0.55,
+    objectFit: 'cover',
+    height: `calc(100vh - ${rhythm(16)} - ${navHeight})`,
+    position: 'absolute',
+    top: navHeight,
+    left: 0,
+    [breakpointMin('tabletLarge')]: {
+      height: `calc(100vh - ${navHeight})`,
+      left: rhythm(informationWidth + 3),
+      width: `calc(100% - ${rhythm(informationWidth + 3)})`,
+    },
+    [breakpointMin('desktop')]: {
+      left: `calc(${rhythm(informationWidth + 3)} + ${desktopExcessLeft})`,
+      width: `calc(100% - ${rhythm(informationWidth + 3)} - ${desktopExcessLeft})`,
+    },
+  },
+});
+
+const Headers = styled('div')({
   marginBottom: `calc(100vh - ${rhythm(19.75)} - ${navHeight})`,
+  [breakpointMin('tabletLarge')]: {
+    marginBottom: rhythm(1),
+  },
   'h1, h2, h3, h4': {
     position: 'relative',
     zIndex: 1,
     marginBottom: rhythm(0.5),
   },
-  'video, img': {
-    width: '100%',
-    opacity: 0.55,
-    objectFit: 'cover',
-    [breakpointMax('tabletLarge')]: {
-      height: `calc(100vh - ${rhythm(16)} - ${navHeight})`,
-      position: 'absolute',
-      top: navHeight,
-      left: 0,
-    },
-  },
 });
 
 const Hero = styled('section')({
-  display: 'flex',
   minHeight: `calc(100vh - ${navHeight})`,
+  display: 'flex',
   backgroundColor: `${colors.red}`,
   padding: `0 ${rhythm(1)}`,
   paddingTop: rhythm(1),
   color: `${colors.white}`,
-  [breakpointMax('tabletLarge')]: {
-    flexDirection: 'column',
-  },
-  p: {
+  'p:first-of-type': {
     textAlign: 'justify',
+    marginBottom: 'auto',
   },
   button: {
-    marginTop: 'auto',
     marginBottom: rhythm(1),
+  },
+  [breakpointMax('tabletLarge')]: {
+    flexDirection: 'column',
   },
 });
 
@@ -72,15 +102,38 @@ export default class extends React.Component<DefaultPageProps & Props> {
         <Head>
           <script src="/static/mc.js" />
         </Head>
-        {contentControl.ticketControl.description
-        && <TicketAlert ticketControl={contentControl.ticketControl} />
-        }
+        {/*{contentControl.ticketControl.description*/}
+        {/*&& <TicketAlert ticketControl={contentControl.ticketControl} />*/}
+        {/*}*/}
         <Hero>
+          <Information>
+            <Headers>
+              <Header1>Poland 2.0 Summit</Header1>
+              {currentEdition.name
+                && <Header3 uppercase bodyFont dangerouslySetInnerHTML={dangerousSuperscripts(currentEdition.name)} />
+              }
+            </Headers>
+            <ReactMarkdown>{currentEdition.description}</ReactMarkdown>
+            <TextWithIcon>
+              <Icon icon="map-marker" color={`${colors.white}`} iconSize={20} />
+              <IconLabel>{currentEdition.venue && currentEdition.venue.name}</IconLabel>
+            </TextWithIcon>
+            <TextWithIcon>
+              <Icon icon="calendar" color={`${colors.white}`} iconSize={20} />
+              <IconLabel>
+                <time dateTime={currentEdition.startDate}>
+                  {dateString(currentEdition.startDate, currentEdition.endDate)}
+                </time>
+              </IconLabel>
+            </TextWithIcon>
+            <br />
+            {contentControl.ticketControl.onSale ? (
+              <Link href="/tickets">
+                <Button uppercase wide>Get Tickets</Button>
+              </Link>
+            ) : null}
+          </Information>
           <CoverMedia>
-            <Header1>Poland 2.0 Summit</Header1>
-            {currentEdition.name
-            && <Header3 uppercase bodyFont dangerouslySetInnerHTML={dangerousSuperscripts(currentEdition.name)} />
-            }
             {currentEdition.coverPhoto && currentEdition.coverPhoto.url.endsWith('.mp4') ? (
               <video autoPlay loop muted playsInline>
                 <source
@@ -96,21 +149,6 @@ export default class extends React.Component<DefaultPageProps & Props> {
               <img src={currentEdition.coverPhoto.url} alt="Graphical cover of this year's edition" />
             )}
           </CoverMedia>
-          <ReactMarkdown>{currentEdition.description || ''}</ReactMarkdown>
-          <TextWithIcon>
-            <Icon icon="map-marker" color={`${colors.white}`} iconSize={20} />
-            <IconLabel>{currentEdition.venue && currentEdition.venue.name}</IconLabel>
-          </TextWithIcon>
-          <TextWithIcon>
-            <Icon icon="calendar" color={`${colors.white}`} iconSize={20} />
-            <IconLabel>
-              <time dateTime={currentEdition.startDate}>
-                {dateString(currentEdition.startDate, currentEdition.endDate)}
-              </time>
-            </IconLabel>
-          </TextWithIcon>
-          <br />
-          <Button uppercase wide>Get Tickets</Button>
         </Hero>
         <Background>
           {contentControl.showPreviousSponsors
