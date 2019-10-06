9c225d1b7144cdea8a5f050933a7ac6e950d1c50
Artur Komoter <artur@komoter.pl>
2019-10-06 14:42:26 +0100

    Increase horizontal padding for Container

--- components/atoms/Container.tsx
+++ components/atoms/Container.tsx
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
+import { rhythm } from '../typography';
 
 const Container = styled('div')({
   maxWidth: 1280,
   margin: '0 auto',
-  padding: '0 1rem',
+  padding: `0 ${rhythm(1)}`,
 });
 
 export default Container;
